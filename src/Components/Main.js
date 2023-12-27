bc31ee341c476a81938b50b5b1be59803e40a9c8
BauteNonSinger <bautista19298@gmail.com>
2023-12-28 00:07:56 +0100

    delete WIP

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Main = () => {
-    return (
-      <div className="main-container">
-        <div className="title-container">
-          <h1>TODO List</h1>
-        </div>
-        <div className="box-container">
-          <form className="new-task-container">
-            <label className="input-title">New task</label>
-            <div className='form'>
-              <input className='new-task-input' type="text" />
-              <div className="form-buttons">
-                <button type="submit" className='button'>Add</button>
-                <button type="cancel" className='button'>Cancel</button>
-              </div>
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const sendTask = (e) => {
+    e.preventDefault();
+    if (task === "") return;
+    taskList.push(task);
+    console.log(taskList);
+    setTask("");
+  };
+
+  const resetForm = (e) => {
+    e.preventDefault();
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    let tempTaskList = taskList;
+    tempTaskList.splice(index, 1);
+    setTaskList(tempTaskList);
+  };
+
+  return (
+    <div className="main-container">
+      <div className="title-container">
+        <h1>TODO List</h1>
+      </div>
+      <div className="box-container">
+        <form className="new-task-container">
+          <label className="input-title">New task</label>
+          <div className="form">
+            <input
+              className="new-task-input"
+              type="text"
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
+            />
+            <div className="form-buttons">
+              <button
+                type="submit"
+                className="button submit"
+                onClick={(e) => sendTask(e)}
+              >
+                Add
+              </button>
+              <button className="button cancel" onClick={(e) => resetForm(e)}>
+                Cancel
+              </button>
             </div>
-          </form>
-          <div className="tasks-container"></div>
+          </div>
+        </form>
+        <div className="tasks-container">
+          {taskList.map((taskItem, index) => {
+            return (
+              <div className="task-component" key={index}>
+                <p className="task-text">{taskItem}</p>
+                <div className="form-buttons task-buttons">
+                  <button className="button submit">Edit</button>
+                  <button
+                    className="button cancel"
+                    onClick={() => deleteTask(index)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Main;
